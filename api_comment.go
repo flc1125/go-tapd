1d8d45b5218db5bcd21d8df46cbc67e7d218dca4
Flc゛ <four_leaf_clover@foxmail.com>
2024-08-28 23:13:37 +0800

    feat(comment): Added CreateComment,GetCommentsCount,UpdateComment

--- api_comment.go
+++ api_comment.go
@@ -48,7 +48,38 @@ func NewCommentService(client *Client) *CommentService {
 	return &CommentService{client}
 }
 
-// 添加评论接口
+type CreateCommentRequest struct {
+	Title       *string           `json:"title,omitempty"`        // 标题
+	Description *string           `json:"description,omitempty"`  // 内容
+	Author      *string           `json:"author,omitempty"`       // 评论人
+	EntryType   *CommentEntryType `json:"entry_type,omitempty"`   // 评论类型
+	EntryID     *int              `json:"entry_id,omitempty"`     // 评论所依附的业务对象实体id
+	ReplyID     *int              `json:"reply_id,omitempty"`     // 评论回复的ID
+	RootID      *int              `json:"root_id,omitempty"`      // 根评论ID
+	WorkspaceID *int              `json:"workspace_id,omitempty"` // 项目ID
+}
+
+// CreateComment 添加评论接口
+//
+// https://open.tapd.cn/document/api-doc/API%E6%96%87%E6%A1%A3/api_reference/comment/add_comment.html
+func (s *CommentService) CreateComment(
+	ctx context.Context, request *CreateCommentRequest, opts ...RequestOption,
+) (*Comment, *Response, error) {
+	req, err := s.client.NewRequest(ctx, http.MethodPost, "comments", request, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var response struct {
+		Comment *Comment `json:"Comment"`
+	}
+	resp, err := s.client.Do(req, &response)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return response.Comment, resp, nil
+}
 
 type GetCommentsRequest struct {
 	// 评论ID 支持多ID查询
@@ -124,5 +155,86 @@ func (s *CommentService) GetComments(
 	return comments, resp, nil
 }
 
-// 获取评论数量
-// 更新评论接口
+type GetCommentsCountRequest struct {
+	// 评论ID 支持多ID查询
+	ID *ID `url:"id,omitempty"`
+
+	// 标题
+	Title *string `url:"title,omitempty"`
+
+	// 内容
+	Description *string `url:"description,omitempty"`
+
+	// 评论人
+	Author *string `url:"author,omitempty"`
+
+	// 评论类型（取值： bug、 bug_remark （流转缺陷时候的评论）、 stories、 tasks 。多个类型间以竖线隔开） 支持枚举查询
+	EntryType *CommentEntryType `url:"entry_type,omitempty"`
+
+	// 评论所依附的业务对象实体id
+	EntryID *int `url:"entry_id,omitempty"`
+
+	// 创建时间 支持时间查询
+	Created *string `url:"created,omitempty"`
+
+	// 最后更改时间 支持时间查询
+	Modified *string `url:"modified,omitempty"`
+
+	// 项目ID
+	WorkspaceID *int `url:"workspace_id,omitempty"`
+
+	// 根评论ID
+	RootID *int `url:"root_id,omitempty"`
+
+	// 评论回复的ID
+	ReplyID *int `url:"reply_id,omitempty"`
+}
+
+// GetCommentsCount 获取评论数量
+//
+// https://open.tapd.cn/document/api-doc/API%E6%96%87%E6%A1%A3/api_reference/comment/get_comments_count.html
+func (s *CommentService) GetCommentsCount(
+	ctx context.Context, request *GetCommentsCountRequest, opts ...RequestOption,
+) (int, *Response, error) {
+	req, err := s.client.NewRequest(ctx, http.MethodGet, "comments/count", request, opts)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	var response CountResponse
+	resp, err := s.client.Do(req, &response)
+	if err != nil {
+		return 0, resp, err
+	}
+
+	return response.Count, resp, nil
+}
+
+type UpdateCommentRequest struct {
+	WorkspaceID   *int    `json:"workspace_id,omitempty"`   // [必须]项目ID
+	ID            *int    `json:"id,omitempty"`             // [必须]评论ID
+	Description   *string `json:"description,omitempty"`    // [必须]内容
+	ChangeCreator *string `json:"change_creator,omitempty"` // 变更人
+}
+
+// UpdateComment 更新评论接口
+//
+// https://open.tapd.cn/document/api-doc/API%E6%96%87%E6%A1%A3/api_reference/comment/update_comment.html
+func (s *CommentService) UpdateComment(
+	ctx context.Context, request *UpdateCommentRequest, opts ...RequestOption,
+) (*Comment, *Response, error) {
+	req, err := s.client.NewRequest(ctx, http.MethodPost, "comments", request, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var response struct {
+		Comment *Comment `json:"Comment"`
+	}
+	resp, err := s.client.Do(req, &response)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return response.Comment, resp, nil
+}
--- api_comment_test.go
+++ api_comment_test.go
@@ -1,11 +1,73 @@
 package tapd
 
 import (
+	"encoding/json"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestCommentService_CommentEntryType(t *testing.T) {
+	assert.Equal(t, "bug", CommentEntryTypeBug.String())
+	assert.Equal(t, "bug_remark", CommentEntryTypeBugRemark.String())
+	assert.Equal(t, "stories", CommentEntryTypeStories.String())
+	assert.Equal(t, "tasks", CommentEntryTypeTasks.String())
+	assert.Equal(t, "wiki", CommentEntryTypeWiki.String())
+}
+
+func TestCommentService_CreateComment(t *testing.T) {
+	_, client := createServerClient(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "/comments", r.URL.Path)
+
+		var req struct {
+			Title       string           `json:"title"`
+			Description string           `json:"description"`
+			Author      string           `json:"author"`
+			EntryType   CommentEntryType `json:"entry_type"`
+			EntryID     int              `json:"entry_id"`
+			ReplyID     int              `json:"reply_id"`
+			RootID      int              `json:"root_id"`
+			WorkspaceID int              `json:"workspace_id"`
+		}
+		assert.NoError(t, json.NewDecoder(r.Body).Decode(&req))
+		assert.Equal(t, "title", req.Title)
+		assert.Equal(t, "description", req.Description)
+		assert.Equal(t, "author", req.Author)
+		assert.Equal(t, CommentEntryTypeStories, req.EntryType)
+		assert.Equal(t, 123, req.EntryID)
+		assert.Equal(t, 0, req.ReplyID)
+		assert.Equal(t, 111, req.RootID)
+		assert.Equal(t, 111, req.WorkspaceID)
+
+		_, _ = w.Write(loadData(t, ".testdata/api/comment/create_comment.json"))
+	}))
+
+	comment, _, err := client.CommentService.CreateComment(ctx, &CreateCommentRequest{
+		Title:       Ptr("title"),
+		Description: Ptr("description"),
+		Author:      Ptr("author"),
+		EntryType:   Ptr(CommentEntryTypeStories),
+		EntryID:     Ptr(123),
+		ReplyID:     Ptr(0),
+		RootID:      Ptr(111),
+		WorkspaceID: Ptr(111),
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, comment)
+	assert.Equal(t, "1111112222001033109", comment.ID)
+	assert.Equal(t, "test title", comment.Title)
+	assert.Equal(t, "test description", comment.Description)
+	assert.Equal(t, "go-tapd", comment.Author)
+	assert.Equal(t, CommentEntryTypeStories, comment.EntryType)
+	assert.Equal(t, "1111112222001071295", comment.EntryID)
+	assert.Equal(t, "0", comment.ReplyID)
+	assert.Equal(t, "1111112222001033105", comment.RootID)
+	assert.Equal(t, "2024-08-28 23:03:06", comment.Created)
+	assert.Equal(t, "2024-08-28 23:03:06", comment.Modified)
+	assert.Equal(t, "11112222", comment.WorkspaceID)
+}
 
 func TestCommentService_GetComments(t *testing.T) {
 	_, client := createServerClient(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -61,4 +123,82 @@ func TestCommentService_GetComments(t *testing.T) {
 	assert.Equal(t, "2024-08-28 10:06:24", comments[0].Created)
 	assert.Equal(t, "2024-08-28 10:06:24", comments[0].Modified)
 	assert.Equal(t, "11112222", comments[0].WorkspaceID)
+}
+
+func TestCommentService_GetCommentsCount(t *testing.T) {
+	_, client := createServerClient(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, "/comments/count", r.URL.Path)
+		assert.Equal(t, "111,222", r.URL.Query().Get("id"))
+		assert.Equal(t, "test title", r.URL.Query().Get("title"))
+		assert.Equal(t, "test description", r.URL.Query().Get("description"))
+		assert.Equal(t, "test author", r.URL.Query().Get("author"))
+		assert.Equal(t, CommentEntryTypeStories.String(), r.URL.Query().Get("entry_type"))
+		assert.Equal(t, "123", r.URL.Query().Get("entry_id"))
+		assert.Equal(t, "2024-08-28", r.URL.Query().Get("created"))
+		assert.Equal(t, "2024-08-28", r.URL.Query().Get("modified"))
+		assert.Equal(t, "111", r.URL.Query().Get("workspace_id"))
+		assert.Equal(t, "222", r.URL.Query().Get("root_id"))
+		assert.Equal(t, "333", r.URL.Query().Get("reply_id"))
+
+		_, _ = w.Write(loadData(t, ".testdata/api/comment/get_comments_count.json"))
+	}))
+
+	count, _, err := client.CommentService.GetCommentsCount(ctx, &GetCommentsCountRequest{
+		ID:          NewID(111, 222),
+		Title:       Ptr("test title"),
+		Description: Ptr("test description"),
+		Author:      Ptr("test author"),
+		EntryType:   Ptr(CommentEntryTypeStories),
+		EntryID:     Ptr(123),
+		Created:     Ptr("2024-08-28"),
+		Modified:    Ptr("2024-08-28"),
+		WorkspaceID: Ptr(111),
+		RootID:      Ptr(222),
+		ReplyID:     Ptr(333),
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, count)
+	assert.Equal(t, 2284, count)
+}
+
+func TestCommentService_UpdateComment(t *testing.T) {
+	_, client := createServerClient(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "/comments", r.URL.Path)
+
+		var req struct {
+			WorkspaceID   int    `json:"workspace_id"`
+			ID            int    `json:"id"`
+			Description   string `json:"description"`
+			ChangeCreator string `json:"change_creator"`
+		}
+		assert.NoError(t, json.NewDecoder(r.Body).Decode(&req))
+		assert.Equal(t, 111, req.WorkspaceID)
+		assert.Equal(t, 111, req.ID)
+		assert.Equal(t, "test description 2", req.Description)
+		assert.Equal(t, "test creator", req.ChangeCreator)
+
+		_, _ = w.Write(loadData(t, ".testdata/api/comment/update_comment.json"))
+	}))
+
+	comment, _, err := client.CommentService.UpdateComment(ctx, &UpdateCommentRequest{
+		WorkspaceID:   Ptr(111),
+		ID:            Ptr(111),
+		Description:   Ptr("test description 2"),
+		ChangeCreator: Ptr("test creator"),
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, comment)
+	assert.Equal(t, "1111112222001033109", comment.ID)
+	assert.Equal(t, "test title", comment.Title)
+	assert.Equal(t, "test description", comment.Description)
+	assert.Equal(t, "go-tapd", comment.Author)
+	assert.Equal(t, CommentEntryTypeStories, comment.EntryType)
+	assert.Equal(t, "1111112222001071295", comment.EntryID)
+	assert.Equal(t, "0", comment.ReplyID)
+	assert.Equal(t, "1111112222001033105", comment.RootID)
+	assert.Equal(t, "2024-08-28 23:03:06", comment.Created)
+	assert.Equal(t, "2024-08-28 23:03:06", comment.Modified)
+	assert.Equal(t, "11112222", comment.WorkspaceID)
 }
